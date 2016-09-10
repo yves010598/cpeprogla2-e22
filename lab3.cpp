f5d8806f281253cea90ddadc606f25db13257d26
YVES ASIDILLO <YJASIDILLO@fit.edu.ph>
2016-09-10 11:33:08 +0800

    Adds custom strcpy

--- lab3.cpp
+++ lab3.cpp
@@ -2,14 +2,32 @@
 #include<cstring>
 using namespace std;
 
-const char* yo(const char* str){
-  //return strlen(str);
-  return str;
+char* myStrCmp( char* srt1, char* srt2){
+	
+switch (strcmp (srt1, srt2)){
+	case -1:
+		cout<<"Negative";
+		break;
+	case 0:
+		cout<<"Equal";
+		break;
+	case 1:
+		cout<<"Positive";
+		break;
+	}
+}
+
+void myStrCpy(char* str1, char* str2){
+
+strcpy(str1, str2);
 }
 
 int main(){
-  char str1[100];
-  char str2[100];
-  strcpy(str1,"Hello there");
-  cout << yo(str1);
+	char str1[100]="Hello";
+	char str2[100]="hello";
+	
+	myStrCmp(str1, str2);
+	myStrCpy(str1, str2);
+	cout << str1;
+	
 }
