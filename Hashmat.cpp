b53bfb78d82179ec87ee1547379bc0b3230f4cc4
Yves Asidillo <yvesasidillo@gmail.com>
2016-09-26 20:21:30 +0800

    Create Hashmat.cpp

---
+++ Hashmat.cpp
@@ -0,0 +1,22 @@
+//ASIDILLO, YVES J
+//201430067
+//CPEPROG2LA
+
+
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+	int a;
+	int b;
+	int c;
+	cout<<"Hashmat Allies:";
+	cin>>a;
+	cout<<"Hashmat Enemy:";
+	cin>>b;
+	
+	c = abs(a-b);
+	cout<<c;
+}
